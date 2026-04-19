2289a243e2d6a2ecbf237a57f33096f4b71347ae
Ademo gure <jewargure1@gmail.com>
2026-04-19 16:05:31 +0200

    chore: remove audio backup download route

--- app.py
+++ app.py
@@ -694,11 +694,6 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 STATIC_UPLOADS_FOLDER = os.path.join(app.static_folder, "uploads")
 os.makedirs(STATIC_UPLOADS_FOLDER, exist_ok=True)
 
-@app.route("/download-audio")
-def download_audio():
-    from flask import send_file
-    return send_file("/tmp/render_full_audio.tar.gz", as_attachment=True)
-
 
 def _copy_static_uploads_to_persistent_startup():
     """
